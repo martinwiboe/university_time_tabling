e915052bb4a56a172369332562fd43a25ea1f21a
Martin Wiboe <martin@substans.com>
2015-04-03 20:34:30 +0200

    Some cleanup in the Curriculum  class Ignored Java build output and removed from repository

--- src/Curriculum.java
+++ src/Curriculum.java
@@ -13,42 +13,47 @@ public class Curriculum {
     public void loadFromFile(String curriculaFile, String relationFile, int numberOfCourses) {
         // Load the list of curricula
         try {
-            Reader r = new FileReader(curriculaFile);
-            BufferedReader in = new BufferedReader(r);
+            // Parse the curricula file
+            // Each line has the curriculum number and course count
+            Reader reader = new FileReader(curriculaFile);
+            BufferedReader bufferedReader = new BufferedReader(reader);
             Pattern p = Pattern.compile("Q(\\d{4}) (\\d)");
             String latestLine;
             int curriculaCount = 0;
-            while ((latestLine = in.readLine()) != null) {
+            while ((latestLine = bufferedReader.readLine()) != null) {
                 Matcher m = p.matcher(latestLine);
                 if (!m.matches())
                     continue;
-                curriculaCount = 1 + Integer.parseInt(m.group(1));
+
+                // Index of the curriculum we just parsed
+                int curriculumIndex = Integer.parseInt(m.group(1));
+
+                // The number of curricula is always equal to highest index + 1
+                curriculaCount = 1 + curriculumIndex;
             }
+            bufferedReader.close();
+            reader.close();
 
+            // Store assignments as an array of booleans
             isCourseInCurriculum = new boolean[numberOfCourses][curriculaCount];
-            in.close();
-            r.close();
 
+            // Parse the relation file
+            // Each line has the index of a curriculum and a course
             Pattern relationPattern = Pattern.compile("Q(\\d{4}) C(\\d{4})");
-            r = new FileReader(relationFile);
-            in = new BufferedReader(r);
-            while ((latestLine = in.readLine()) != null) {
+            reader = new FileReader(relationFile);
+            bufferedReader = new BufferedReader(reader);
+            while ((latestLine = bufferedReader.readLine()) != null) {
                 Matcher m = relationPattern.matcher(latestLine);
                 if (!m.matches())
                     continue;
 
+                // Indices we just read from the file
                 int curriculumId = Integer.parseInt(m.group(1));
                 int courseId = Integer.parseInt(m.group(2));
                 isCourseInCurriculum[courseId][curriculumId] = true;
             }
-
-            System.out.println("Max curriculum: " + ((Integer)curriculaCount).toString());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
 }
